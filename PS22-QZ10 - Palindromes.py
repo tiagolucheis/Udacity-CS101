75ab70fcd662548fa0c54a6c225d2980e76a1f5a
Tiago Lucheis <tiagolucheis@gmail.com>
2018-09-01 12:26:46 -0300

    Recursive Exercises added.

---
+++ PS22-QZ10 - Palindromes.py
@@ -0,0 +1,31 @@
+# Define a procedure is_palindrome, that takes as input a string, and returns a
+# Boolean indicating if the input string is a palindrome.
+
+# Base Case: '' => True
+# Recursive Case: if first and last characters don't match => False
+# if they do match, is the middle a palindrome?
+
+def is_palindrome(s):
+
+    if len(s) <= 1:
+        
+        result = True
+        
+    elif s[0] == s[-1]:
+        
+        result = is_palindrome(s[1:len(s)-1])
+    
+    else:
+        
+        result = False
+    
+    return result
+ 
+ 
+ 
+print(is_palindrome(''))
+#>>> True
+print(is_palindrome('abab'))
+#>>> False
+print(is_palindrome('abba'))
+#>>> True
